dfbc4300d8cdb7741bda0b6e31b9021fa15f0185
apps <23049337+yevgenis-shapiro-max2006@users.noreply.github.com>
2026-09-24 13:51:41 +0300

    update

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket = "apps-terraform-clusters"
-    key    = "eks-sonarqube/terraform.tfstate"
+    key    = "eks-web3/terraform.tfstate"
     region = "eu-central-1"
   }
 }
